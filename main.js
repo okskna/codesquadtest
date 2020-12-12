240f73fcbeeaa183027353305541ad7d72111104
Sunkwan Kim <skyvlu@gmail.com>
2020-12-12 16:05:23 +0900

    Add console input method
    * nodejs 의 readline을 이용하여 console input 메소드를 구현함

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("Hello world!");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const userInput = () => {
+  let ret = [];
+  rl.on('line', (input) => {
+    console.log(`Received: ${input}`);
+    ret = input.split(' ');
+    rl.emit('myEvent', ret);
+  }).on('SIGINT', () => {
+    rl.question('Are you sure you want to exit? ', (answer) => {
+      if (answer.match(/^y(es)?$/i)) rl.pause();
+    });
+  });
+
+  return ret;
+}
+
+rl.on('myEvent', (param) => {
+  console.log("Event work!: ", param);
+})
+
+
+const main = () => {
+  userInput();
+  
+}
+
+main();
