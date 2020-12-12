bf6f36e840d092d013549afc9e3f7750ca689807
Sunkwan Kim <skyvlu@gmail.com>
2020-12-13 03:34:52 +0900

    Add pushString method * readline 이벤트에 새로운 이벤트인 pushString을 추가하여 구현. * readline의 preprocessing 이벤트가 종료 시 pushString 이벤트를 emit한다. * 기본적인 동작 확인 완료.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const userInput = () => {
   rl.on('line', (input) => {
     console.log(`Received: ${input}`);
     ret = input.split(' ');
-    rl.emit('myEvent', ret);
+    rl.emit('preprocessing', ret);
     rl.prompt();
   }).on('SIGINT', () => {
     rl.question('Are you sure you want to exit? ', (answer) => {
@@ -21,13 +21,6 @@ const userInput = () => {
 
   return ret;
 }
-
-rl.on('myEvent', (param) => {
-  console.log("Event work!: ", param);
-  inputPreprocessing(...param);
-});
-
-
 
 const inputPreprocessing = (...arg) => {
   try {
@@ -47,6 +40,7 @@ const inputPreprocessing = (...arg) => {
     else          throw('Direction is not a valid value.')
 
     console.log('Res: ', word, numb, direction);
+    return [word, numb, direction];
   } catch (msg) {
     console.log('Error(pushString): ', msg);
   }
@@ -56,5 +50,37 @@ const main = () => {
   userInput();
   
 }
+
+rl.on('pushString', (ops) => {
+  console.log("pushString: ", ops);
+
+  let word, numb, direction;
+  [word, numb, direction] = ops;
+
+  direction = direction === 'L' ? -1 : 1;
+
+  const wordLength = word.length;
+  const popTimes = (numb * direction) % wordLength;
+
+  Array(Math.abs(popTimes)).fill(0).forEach( () => {
+    if (popTimes < 0) {
+      let firstItem = word.shift();
+      word.push(firstItem);
+    } else if (popTimes > 0) {
+      let lastItem = word.pop();
+      word.unshift(lastItem);
+    }
+  });
+
+  console.log(word);
+});
+
+rl.on('preprocessing', (param) => {
+  console.log("Event work!: ", param);
+
+  const ops = inputPreprocessing(...param);
+  rl.emit('pushString', ops);
+});
+
 
 main();
