1148ffb4d0fb63b85d08e6410aa275dcb5659e64
Sunkwan Kim <skyvlu@gmail.com>
2020-12-12 17:46:53 +0900

    Add user input preprocessing method * 인자를 충분히 입력하지 않을 경우 재입력을 요구하는 처리를 하였음. * 숫자를 입력할 차례에 숫자가 아닌 것을 입력할 경우 재입력을 요구. * 방향을 입력할 차례에 잘못된 방향을 입력할 경우 재입력을 요구.

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: '> '
 });
 
 const userInput = () => {
   let ret = [];
+  rl.prompt();
   rl.on('line', (input) => {
     console.log(`Received: ${input}`);
     ret = input.split(' ');
     rl.emit('myEvent', ret);
+    rl.prompt();
   }).on('SIGINT', () => {
     rl.question('Are you sure you want to exit? ', (answer) => {
       if (answer.match(/^y(es)?$/i)) rl.pause();
@@ -21,8 +24,33 @@ const userInput = () => {
 
 rl.on('myEvent', (param) => {
   console.log("Event work!: ", param);
-})
+  inputPreprocessing(...param);
+});
 
+
+
+const inputPreprocessing = (...arg) => {
+  try {
+    let word, numb, direction;
+    [word, numb, direction] = arg;
+
+    if (!word || !numb || !direction) throw('Not enough arguments.');
+    
+    word = word.split('');
+    const numbInt = parseInt(numb);
+    if (numbInt === numbInt) numb = parseInt(numb);
+    else                     throw('Numb is not a valid value.');
+
+    // console.log(direction.match(/^(l|r)(eft|ight)?$/i));
+    const matchDir = direction.match(/^(l|r)(eft|ight)?$/i);
+    if (matchDir) direction = matchDir[1].toUpperCase();
+    else          throw('Direction is not a valid value.')
+
+    console.log('Res: ', word, numb, direction);
+  } catch (msg) {
+    console.log('Error(pushString): ', msg);
+  }
+}
 
 const main = () => {
   userInput();
